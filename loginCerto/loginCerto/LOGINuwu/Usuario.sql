996539d463ae1bbeb0ce9bbf96f336972ddbd736
Anna Júlia do Prado <147573503+jujobs@users.noreply.github.com>
2025-05-16 12:52:10 -0300

    Delete loginCerto directory

--- loginCerto/loginCerto/LOGINuwu/Usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE usuarioetimpwiii;
-USE usuarioetimpwiii;
-CREATE TABLE Usuarios (
-    id    INT AUTO_INCREMENT PRIMARY KEY,
-    nome  VARCHAR(50)  NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(32)  NOT NULL
-)

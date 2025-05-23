385f5ff13da5d4496da3abb7a4639a6e4089a086
Anna Júlia do Prado <147573503+jujobs@users.noreply.github.com>
2025-05-23 11:35:02 -0300

    Delete loginfofis directory

--- loginfofis/Usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE Usuarioetimpwiii;
-USE Usuarioetimpwiii;
-CREATE TABLE Usuarios (
-    id    INT AUTO_INCREMENT PRIMARY KEY,
-    nome  VARCHAR(50)  NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(32)  NOT NULL
-)
